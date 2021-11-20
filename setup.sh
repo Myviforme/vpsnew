8da14abc2d93e56c4bc109e4b7196384e101091b
Myviforme <88879341+Myviforme@users.noreply.github.com>
2021-11-20 22:14:01 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-if [ "${EUID}" -ne 0 ]; then
-		echo "You need to run this script as root"
-		exit 1
-fi
-if [ "$(systemd-detect-virt)" == "openvz" ]; then
-		echo "OpenVZ is not supported"
-		exit 1
 
-if [ -f "/etc/v2ray/domain" ]; then
-echo "Script Already Installed"
-exit 0
-fi
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
 wget https://raw.githubusercontent.com/Myviforme/vpsnew/main/cf.sh && chmod +x cf.sh && ./cf.sh
